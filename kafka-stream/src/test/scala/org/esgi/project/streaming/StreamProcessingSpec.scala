51d52cd2e7a0ef45b66589b7782c509c0593bc1a
Elisabeth <e.fauquet3@gmail.com>
2024-07-18 12:00:30 +0200

    view count window test last 5 min ok

--- kafka-stream/src/test/scala/org/esgi/project/streaming/StreamProcessingSpec.scala
+++ kafka-stream/src/test/scala/org/esgi/project/streaming/StreamProcessingSpec.scala
@@ -3,28 +3,28 @@ package org.esgi.project.streaming
 import io.github.azhur.kafka.serde.PlayJsonSupport
 import org.apache.kafka.streams.TopologyTestDriver
 import org.apache.kafka.streams.scala.serialization.Serdes
-import org.apache.kafka.streams.state.KeyValueStore
+import org.apache.kafka.streams.state.{KeyValueStore, WindowStore}
 import org.apache.kafka.streams.test.TestRecord
 import org.esgi.project.api.models.{Like, View}
 import org.scalatest.funsuite.AnyFunSuite
 
+import java.time.temporal.ChronoUnit
+import java.time.{Duration, Instant}
 import scala.jdk.CollectionConverters._
 
 class StreamProcessingSpec extends AnyFunSuite with PlayJsonSupport {
-  test("Topology should compute a correct word count") {
+  test("Topology should compute a correct view and like count") {
     // Given
-    val messages = List(
-      "hello world",
-      "hello moon",
-      "foobar",
-      "42"
-    )
-
     val views = List[View](
       View(1, "Kill Bill", "half"),
       View(1, "Kill Bill", "half"),
       View(1, "Kill Bill", "full"),
       View(2, "Matrix", "full")
+    )
+
+    val newViews = List[View](
+      View(1, "Kill Bill", "half"),
+      View(2, "Matrix", "half")
     )
 
     val likes = List[Like](
@@ -57,26 +57,52 @@ class StreamProcessingSpec extends AnyFunSuite with PlayJsonSupport {
           StreamProcessingTest.viewCountByIdCategoryStorename
         )
 
-    val viewCountWindowedStore: KeyValueStore[String, Long] =
+    val viewCountWindowedStore: WindowStore[String, Long] =
       topologyTestDriver
-        .getKeyValueStore[String, Long](
+        .getWindowStore[String, Long](
           StreamProcessingTest.viewCountByWindowedIdAndCategoryStore
         )
 
     // When
+    val startTime = Instant.now()
+
     viewTopic.pipeRecordList(
       views.map(view => new TestRecord(view.id, view)).asJava
     )
+    topologyTestDriver.advanceWallClockTime(Duration.ofMinutes(5))
+
+    val windowedStartTime = startTime.plus(Duration.ofMinutes(5)).truncatedTo(ChronoUnit.MINUTES)
+    val windowedEndTime = windowedStartTime.plus(Duration.ofMinutes(5))
+    val recordTime = windowedStartTime.plus(Duration.ofSeconds(1))
+
+    viewTopic.pipeRecordList(
+      newViews.map(view => new TestRecord(view.id, view, recordTime)).asJava
+    )
+    topologyTestDriver.advanceWallClockTime(Duration.ofMinutes(5))
 
     // Then
-    assert(viewCountStore.get("1-half") == 2)
+    assert(viewCountStore.get("1-half") == 3)
     assert(viewCountStore.get("1-full") == 1)
-    assert(viewCountStore.get("2-half") == 0)
-//    assert(viewCountWindowedStore.get("2-half") == 0)
-//    assert(viewCountStore.get("hello") == 2)
-//    assert(viewCountStore.get("world") == 1)
-//    assert(viewCountStore.get("moon") == 1)
-//    assert(viewCountStore.get("foobar") == 1)
-//    assert(viewCountStore.get("42") == 1)
+    assert(viewCountStore.get("2-half") == 1)
+    assert(viewCountStore.get("2-full") == 1)
+
+
+    val windowedResults1Half =
+      fetchWindowedResults(viewCountWindowedStore, "1-half", windowedStartTime, windowedEndTime)
+    val windowedResults1Full =
+      fetchWindowedResults(viewCountWindowedStore, "1-full", windowedStartTime, windowedEndTime)
+    val windowedResults2Full =
+      fetchWindowedResults(viewCountWindowedStore, "2-full", windowedStartTime, windowedEndTime)
+    val windowedResults2Half =
+      fetchWindowedResults(viewCountWindowedStore, "2-half", windowedStartTime, windowedEndTime)
+
+    assert(windowedResults1Half == 1)
+    assert(windowedResults1Full == 0)
+    assert(windowedResults2Full == 0)
+    assert(windowedResults2Half == 1)
+  }
+  def fetchWindowedResults(store: WindowStore[String, Long], key: String, from: Instant, to: Instant): Long = {
+    val iterator = store.fetch(key, from, to)
+    iterator.asScala.foldLeft(0L)((agg, kv) => agg + kv.value)
   }
 }
